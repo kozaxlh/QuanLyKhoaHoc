c00e00cda8cfb4e85684966cb10f345dffc40f35
Admin <Admin@Admin-PC>
2023-03-04 23:31:25 +0700

    add CourseDLL method

--- src/main/java/BLL/CourseBLL.java
+++ src/main/java/BLL/CourseBLL.java
@@ -25,4 +25,10 @@ public class CourseBLL {
     public boolean deleteCourse(Course course){
         return courseDAO.deleteCourse(course);
     }
+    public void addCrouse(Course course) {
+        courseDAO.addCourse(course);
+    }
+    public ArrayList<Course> getCourse(int id) throws SQLException {
+        return courseDAO.getCourse(id);
+    }
 }
