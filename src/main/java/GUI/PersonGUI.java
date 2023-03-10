ef4fa20c6ee1a84ab2fa73509e2f1b1cf4091287
nam050402 <114125279+nam050402@users.noreply.github.com>
2023-03-10 19:41:02 +0700

    Add files via upload

--- src/main/java/GUI/PersonGUI.java
+++ src/main/java/GUI/PersonGUI.java
@@ -4,8 +4,14 @@
  */
 package GUI;
 
+import BLL.CourseBLL;
 import BLL.PersonBLL;
+import BLL.StudentGradeBLL;
+import Entity.Course;
+import Entity.CourseInstructor;
 import Entity.Person;
+import Entity.StudentGrade;
+import com.mycompany.BLL.CourseInstructorBLL;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -16,32 +22,98 @@ import java.util.Scanner;
  */
 public class PersonGUI {
 
-    PersonBLL bll = new PersonBLL();
-
-    public void showScreen() throws SQLException {
+    static PersonBLL bll = new PersonBLL();
+    public void showScreen(int courseID) throws SQLException {
         Scanner sc = new Scanner(System.in);
+        CourseInstructorBLL courseInstructorBLL = new CourseInstructorBLL();
+        StudentGradeBLL studentGradeBLL = new StudentGradeBLL();
         int choice = -1;
-
         do {
-            System.out.println("============Cap nhat giang day=============");
+            System.out.println("===========Danh sach giang vien============");
+            try {
+                showInstructorList(courseInstructorBLL.getCourseInstructors(courseID));
+            } catch (SQLException ex) {
+                System.out.println("Không thể lấy dữ liệu");
+            }
+            System.out.println("===========Danh sach sinh vien=============");
+            try {
+                showStudentList(studentGradeBLL.getStudentInCourse(courseID));
+            } catch (SQLException ex) {
+                System.out.println("Không thể lấy dữ liệu");
+            }
+            System.out.println("===========Cap nhat giang day==============");
             System.out.println("1:Them giao vien                     ");
-            System.out.println("2:Sua giao vien                    ");
-            System.out.println("3:Xoa giao vien                                  ");
-            System.out.println("4:Them học viên                                     ");
-            System.out.println("5:Xoa hoc vien                          ");
-            System.out.println("6:Nhap diem                         ");
+            System.out.println("2:Xoa giao vien                                  ");
+            System.out.println("3:Them hoc viên                                     ");
+            System.out.println("4:Xoa hoc vien                          ");
+            System.out.println("5:Nhap diem                         ");
             System.out.println("0:Thoat                                             ");
             choice = sc.nextInt();
             switch (choice) {
                 case 1 -> {
-
+                    addInstructorScreen(courseID);
                 }
                 case 2 -> {
-
+                    deleteInstructorScreen(courseID);
                 }
                 case 0 -> {
                 }
             }
         } while (choice != 0);
     }
+    private static void showInstructorList(ArrayList<CourseInstructor> courseInstructors) throws SQLException {
+        System.out.println("Ma khoa hoc|Ma giang vien");
+
+        for (CourseInstructor item : courseInstructors) {
+            System.out.println(String.format("%-11s %s",item.getCourseID(), item.getPersonId()));
+        }
+    }
+    private void showStudentList(ArrayList<StudentGrade> students) throws SQLException {
+        System.out.println("Ma hoc vien|Diem so");
+
+        for (StudentGrade item : students) {
+            System.out.println(String.format("%-11s %s",item.getStudentID(), item.getGrade()));
+        }
+    }
+    private static void addInstructorScreen(int CourseID) throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        PersonBLL instructorInfo = new PersonBLL();
+        ArrayList<Person> instructors = new ArrayList<Person>();
+        instructors = instructorInfo.getInstructors();
+        int choice = -1;
+        int instructorID;
+        
+        do {
+            boolean flag = false;
+            System.out.print("Nhap id giang vien: ");
+            instructorID = sc.nextInt();
+            for (Person person:instructors){
+                if (instructorID == person.getId()){
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag == true){
+                CourseInstructorBLL courseInstructorBLL = new CourseInstructorBLL();
+                courseInstructorBLL.addCourseInstructor(CourseID, instructorID);
+                System.out.println("Them du lieu thanh cong");
+                System.out.println("Nhap 1 de them giang vien hoac nhap 0 de thoat ra ");
+                choice = sc.nextInt();
+            }
+            else{
+                System.out.println("Id giang vien khong ton tai");
+                System.out.println("Nhap 1 de tiep tuc them giang vien hoac nhap 0 de thoat ra ");
+                choice = sc.nextInt();
+            }
+        } while (choice != 0);
+    }
+    private static void deleteInstructorScreen(int CourseID) throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        int instructorID;
+        System.out.print("Nhap id giang vien: ");
+        instructorID = sc.nextInt();
+        CourseInstructorBLL courseInstructorBLL = new CourseInstructorBLL();
+        courseInstructorBLL.deleteCourse(CourseID, instructorID);
+    }
 }
+
