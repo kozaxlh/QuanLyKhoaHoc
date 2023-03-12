a44639fbb655444eb9258625326c861b49293a79
nam050402 <114125279+nam050402@users.noreply.github.com>
2023-03-12 20:46:44 +0700

    Add files via upload

--- src/main/java/BLL/StudentGradeBLL.java
+++ src/main/java/BLL/StudentGradeBLL.java
@@ -25,9 +25,13 @@ public class StudentGradeBLL {
         return studentGradeDAO.getStudentGradeInCourse(id);
     }
 
-    public boolean addStudentGradeInCourse(StudentGrade grade) throws SQLException {
+    public boolean addStudentGrade(StudentGrade grade) throws SQLException {
         checkStudentGradeValid(grade);
-        return studentGradeDAO.addStudentGradeInCourse(grade);
+        return studentGradeDAO.addStudentGrade(grade);
+    }
+    public boolean addStudentGrade(int CourseID, int StudentID, float Grade) throws SQLException {
+        checkStudentGradeValid(Grade);
+        return studentGradeDAO.addStudentGrade(CourseID, StudentID, Grade);
     }
 
     public boolean addStudentGradeInCourse(ArrayList<StudentGrade> gradeList) throws SQLException {
@@ -41,13 +45,27 @@ public class StudentGradeBLL {
         checkStudentGradeValid(grade);
         return studentGradeDAO.updateStudentGrade(grade);
     }
+    public boolean updateStudentGrade(int CourseID, int StudentID, float Grade ) throws SQLException {
+        checkStudentGradeValid(Grade);
+        return studentGradeDAO.updateStudentGrade(CourseID, StudentID, Grade);
+    }
 
     public boolean deleteStudentGrade(StudentGrade grade) throws SQLException {
         return studentGradeDAO.deleteStudentGrade(grade);
     }
+    public boolean deleteStudentGrade(int courseID, int studentID) throws SQLException {
+        return studentGradeDAO.deleteStudentGrade(courseID, studentID);
+    }
     
     private boolean checkStudentGradeValid(StudentGrade grade) {
         if (grade.getGrade() > 4.0 || grade.getGrade() < 0) {
+            throw new RuntimeException("Diem khong hop le. Diem phai tu 0 - 4 diem");
+        }
+        else
+            return true;
+    }
+    private boolean checkStudentGradeValid(float grade) {
+        if (grade > 4.0 || grade < 0) {
             throw new RuntimeException("Diem khong hop le. Diem phai tu 0 - 4 diem");
         }
         else
